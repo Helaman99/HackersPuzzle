4cf35290b923c0e8d1f7c553176c3ed37e46c1c1
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2022-11-05 17:48:12 -0700

    Added Starting Files
    These files are what I have so far, and consist of completed levels 1-5 and a level-in-construction level 6.

---
+++ level4valid.js
@@ -0,0 +1,4 @@
+function validatePass () {
+    if (document.getElementById("password").value == '123456')
+        document.getElementById("nextLevel").innerHTML = "<a href = 'Zd$l-x_nup.html'>Next Level</a>";
+}
---
+++ level5valid.js
@@ -0,0 +1,5 @@
+function validateCreds () {
+    if (document.getElementById("username").value == 'admin'
+        && document.getElementById("password").value == '12345')
+        document.getElementById("nextLevel").innerHTML = "<a href = 'OOF_$3c0ndH@If.html'>Next Level</a>";
+}
